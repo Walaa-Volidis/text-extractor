4d44170d02ab3fffcd47c5807f50f6e6e2cfce18
Walaa VOILIDAS <walaavolidis@gmail.com>
2025-01-27 09:01:14 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ImageUploader } from './components/ImageUploader';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <ImageUploader />
+    </main>
+  );
+}
